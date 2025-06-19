550fb6de6716a73661fe0591febebb92b7bf13b9
AVBKSPhaniKumar <avbksphanikumar121@gmail.com>
2025-06-19 23:13:39 +0530

    added functions to python file

--- neuralnetworks.py
+++ neuralnetworks.py
@@ -1,9 +1,8 @@
 import numpy as np
-import random
 
 
 
-inputs = [[0.1,0.2,-0.4],[0.3,-0.1,5],[0.3,0.5,0.6]]
+
 
 class nn:
     def __init__(self,inputs,neurons):
@@ -19,15 +18,26 @@ class activation_relu:
 
 
 
+class activation_softmax:
+    def forward(self,inputs):
+        self.outputs = np.exp(inputs)/np.sum(np.exp(inputs),axis=1,keepdims=True)
+
+
+inputs = [[0.1,0.2,-0.4],[0.3,-0.1,5],[0.3,0.5,0.6]]   
+
 layer1 = nn(3,4)
 layer1.forward_nn(inputs)
-print(layer1.outputs)
+print("layer1_outputs >> ",layer1.outputs)
 
 layer2 = nn(4,3)
 layer2.forward_nn(layer1.outputs)
-print(layer2.outputs)
+print("layer2_outputs >> ",layer2.outputs)
 
 
 activation1 = activation_relu()
 activation1.forward(layer2.outputs)
-print(activation1.outputs)
+print("activation1_outputs>> ",activation1.outputs)
+
+activation2 = activation_softmax()
+activation2.forward(activation1.outputs)
+print("activation2_outputs >> ",activation2.outputs)
